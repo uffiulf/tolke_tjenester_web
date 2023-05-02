57e2115e1a013e1f692dd62e035ae3da3f13ee78
Uffi <olav.lilje@gmail.com>
2023-05-02 06:21:49 +0200

    nettsiden med farger

--- server.js
+++ server.js
@@ -1,10 +1,8 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+app.use(express.static('public'));
 
-app.get('/', (req, res) => {
-  res.send('Velkommen til tolketjenesten!');
-});
+const port = 3000;
 
 app.listen(port, () => {
   console.log(`Serveren kjører på http://localhost:${port}`);
